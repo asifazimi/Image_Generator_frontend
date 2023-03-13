0467660112ff9923b03626f7829d8025d3308686
Asif Azimi <asifazimi620@hotmail.com>
2023-03-13 20:59:31 +0430

    Add Home and CreatePost pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,29 @@
-import { useState, useRef } from "react";
-import Button from "./components/Button";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { logo } from "./assets";
+import { Home, CreatePost } from "./pages";
 
 const App = () => {
-  const [input, setInput] = useState("");
-  const [lists, setLists] = useState([]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLists([...lists, input]);
-    setInput("");
-  };
-
   return (
     <>
-      <h2>TO DO LIST here</h2>
-      {/* Search */}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="listInput">Task</label>
-        <input
-          type="text"
-          id="listInput"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <input type="button" onClick={handleSubmit} value="Submit" />
-      </form>
-      {/* List */}
-      <ul className="lists">
-        {lists.map((list, index) => {
-          return <li key={index}>{list}</li>;
-        })}
-      </ul>
+      <BrowserRouter>
+        <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4] ">
+          <Link to="/">
+            <img src={logo} alt="Logo" className="w-28 object-contain" />
+          </Link>
+          <Link
+            to="/create-post"
+            className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+          >
+            Create
+          </Link>
+        </header>
+        <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create-post" element={<CreatePost />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </>
   );
 };
